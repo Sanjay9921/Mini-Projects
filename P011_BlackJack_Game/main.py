92c2aea52b1394a7cf6bc79c2e8155640856e70d
Sanjay Prabhu <sanjay.prabhu1729@gmail.com>
2023-04-03 14:35:52 +0530

    Python Games

--- P011_BlackJack_Game/main.py
+++
@@ -1,9 +0,0 @@
-from play_blackjack import *
-
-end_of_game = False
-while not end_of_game:
-    play_game()
-    choice = input("Do you wish to play another game? 'y' or 'n'").casefold()
-    if choice=="n":
-        print("Thank you for playing Blackjack! Do not get addicted to gambling! ")
-        end_of_game = True
--- P012_Number_Guessing_Game/main.py
+++
@@ -1,9 +0,0 @@
-from play_game import *
-
-end_game = False
-while not end_game:
-    play_game()
-    choice = input("Press 'y' to continue to the next game, 'n' to quit..").casefold()
-    if choice=="n":
-        print("Thank you for using our software!")
-        end_game = True
